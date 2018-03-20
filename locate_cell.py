6272805909bb4874df2dcf35566accb5d392598e
Brad Busse <bbusse@gmail.com>
2018-03-20 11:24:08 -0400

    Rename images/timepotins to movies/frames, add framework for displaying code overlays

---
+++ locate_cell.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy import ndimage
+import time
+import skimage.draw
+
+import image_data
+import track_food_vacuole
+
+
+
+
+if __name__ == "__main__":
+
+movgen = image_data.all_movies()
+
+    for movie in movgen:
+        framegen = image_data.all_frames(movie)
+        for frame in framegen:
+            imgplot = plt.imshow(frame[1,:,:])
+            imgplot.set_cmap('gray')
+
+            com_out = np.zeros([frame.shape[1],frame.shape[2]])
+            com, labels, numlabels = track_food_vacuole.find_centroid(frame[1,:,:])
+            #com_out[labels==1] = 1
+            rr,cc = skimage.draw.circle(com[0],com[1],10)
+            com_out[rr,cc] = 1
+
+            print com_out.shape
+            plt.imshow(com_out, alpha=0.25, cmap=plt.get_cmap('Oranges'))
+
+            plt.draw()
+            plt.pause(0.5)
--- track_food_vacuole.py
+++ track_food_vacuole.py
@@ -18,35 +18,36 @@ def find_centroid(img):
             labels[labels==l] = 0
 
     labels, numlabel = ndimage.label(labels)
-    return labels, numlabel
-
-
-for i in range(10):
-    current_image = image_data.fetch_random_image()
-    labels, numlabel = find_centroid(current_image[1,:,:])
-
     com = ndimage.measurements.center_of_mass(np.ones(labels.shape),labels,numlabel)
-    print com
+    return com, labels, numlabel
+
+if __name__ == '__main__':
+    for i in range(10):
+        current_image = image_data.fetch_random_image()
+        com, labels, numlabel = find_centroid(current_image[1,:,:])
+
+        #com = ndimage.measurements.center_of_mass(np.ones(labels.shape),labels,numlabel)
+        print com
 
 
-    plt.imshow(labels)
-    plt.show()
-#
-# for image in imggen:
-#     img = image[0]
-#     print image[1]
-#
-#     for timepoint in range(img.shape[0]):
-#         current_image = img[timepoint,:,:,:].squeeze()
-#
-#         labels, numlabel = find_centroid(current_image[1,:,:])
-#
-#         com = ndimage.measurements.center_of_mass(np.ones(labels.shape),labels,numlabel)
-#         print timepoint, com
-#         #find com of all labels
-#         #pick closest to center
-#
-#         plt.imshow(labels)
+        plt.imshow(labels)
+        plt.show()
+    #
+    # for image in imggen:
+    #     img = image[0]
+    #     print image[1]
+    #
+    #     for timepoint in range(img.shape[0]):
+    #         current_image = img[timepoint,:,:,:].squeeze()
+    #
+    #         labels, numlabel = find_centroid(current_image[1,:,:])
+    #
+    #         com = ndimage.measurements.center_of_mass(np.ones(labels.shape),labels,numlabel)
+    #         print timepoint, com
+    #         #find com of all labels
+    #         #pick closest to center
+    #
+    #         plt.imshow(labels)
 
-        #plt.plot([np.sum(img[i,0,:,:]) for i in range(img.shape[0])])
-#        plt.show()
+            #plt.plot([np.sum(img[i,0,:,:]) for i in range(img.shape[0])])
+    #        plt.show()
