4729399affea68c18fdac4d0f872eeb7f15e442c
Patrick <patrick.allen.fletcher@gmail.com>
2018-03-20 00:23:03 -0400

    scratch work

--- PF_scratch.py
+++ PF_scratch.py
@@ -1,6 +1,8 @@
 import skimage.io
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy import ndimage
+from skimage import filters
 
 import load_images
 
@@ -8,9 +10,24 @@ prefix = '../Curated Images/'
 
 images = load_images.images
 
-img = skimage.io.imread(''.join([prefix,images[0]['filename']]))
-a=img[1,1,:,:]
-b=np.abs(a-np.mean(a.flatten()))
-plt.imshow(b,cmap='gray')
+ix=1
+movie = skimage.io.imread(''.join([prefix,images[ix]['filename']]))
+#print images[ix]['rounded']
+
+A=movie[0,1,:,:]
+
+# B=A-ndimage.gaussian_filter(A,sigma=15)
+
+# B=ndimage.grey_dilation(A,15)-ndimage.gaussian_filter(A,15)
+
+# B=filters.scharr(A)
+
+# thr = filters.threshold_otsu(ndimage.gaussian_filter(A,55))
+# B=A-thr
+
+# B=ndimage.morphological_gradient(A,15)
+
+plt.imshow(A,cmap='gray')
+# plt.imshow(B,cmap='gray')
 
 plt.show()
--- example_active_contour.py
+++ example_active_contour.py
@@ -2,20 +2,22 @@ import skimage.io
 import numpy as np
 import matplotlib.pyplot as plt
 from skimage.segmentation import active_contour
+from skimage.filters import gaussian
 
 import load_images
 prefix = '../Curated Images/'
 images = load_images.images
 
-img = skimage.io.imread(''.join([prefix,images[0]['filename']]))
+movie = skimage.io.imread(''.join([prefix,images[0]['filename']]))
+img=movie[1,1,:,:]
 
 # initial snake
 centr=np.array([250,250])
 rad=100
-si=np.array([centr[0]+rad*np.sin(np.linspace(0,2*np.pi,50)), centr[1]+rad*np.cos(np.linspace(0,2*np.pi,50))])
-si=np.transpose(si)
+theta=np.linspace(0,2*np.pi,50)
+si=np.array([centr[0]+rad*np.sin(theta), centr[1]+rad*np.cos(theta)]).T
 
-s=active_contour(img,si,alpha=0.01,beta=0.01,w_line=1)
+s=active_contour(img,si,alpha=0.01,beta=0.01,w_line=1,max_iterations=5000,convergence=0.1)
 plt.imshow(img,cmap='gray')
 plt.plot(si[:,0],si[:,1])
 plt.plot(s[:,0],s[:,1])
