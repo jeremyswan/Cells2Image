be3d5556988ff1b38a8063ea89d51b48478f68e6
Patrick <patrick.allen.fletcher@gmail.com>
2018-03-20 10:51:32 -0400

    vacuole segment prototype

--- PF_scratch.py
+++ PF_scratch.py
@@ -6,15 +6,21 @@ from skimage import filters
 
 import load_images
 
-prefix = '../Curated Images/'
-
-images = load_images.images
-
-ix=1
-movie = skimage.io.imread(''.join([prefix,images[ix]['filename']]))
+ix=0
+movie = skimage.io.imread(''.join([load_images.prefix,load_images.images[ix]['filename']]))
 #print images[ix]['rounded']
 
-A=movie[0,1,:,:]
+A=movie[68,1,:,:]
+
+B=np.abs(A-np.percentile(A.flatten(),75))
+B=ndimage.gaussian_filter(B,15)
+thr = filters.threshold_otsu(B)
+B=B>thr
+
+L=skimage.measure.label(B)
+RP=skimage.measure.regionprops(L,intensity_image=A,)
+
+print 4*np.pi*RP[0].area/(RP[0].perimeter**2)
 
 # B=A-ndimage.gaussian_filter(A,sigma=15)
 
@@ -27,7 +33,11 @@ A=movie[0,1,:,:]
 
 # B=ndimage.morphological_gradient(A,15)
 
+plt.subplot(1,2,1)
 plt.imshow(A,cmap='gray')
+plt.subplot(1,2,2)
+plt.imshow(A*B,cmap='gray')
 # plt.imshow(B,cmap='gray')
+#plt.imshow(L,cmap='gray')
 
 plt.show()
